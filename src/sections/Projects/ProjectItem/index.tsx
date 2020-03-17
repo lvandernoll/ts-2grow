c17796b8cd3232a3b4e58584260875eb9d03e8f7
lvandernoll <leonvdnoll@gmail.com>
2020-03-17 17:08:42 +0100

    added carousel and popup

--- src/sections/Projects/ProjectItem/index.tsx
+++ src/sections/Projects/ProjectItem/index.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../Projects.module.scss';
 
 interface Props {
   name: string,
-  picture: string,
+  pictures: string[],
 }
 
-const ProjectItem: React.FC<Props> = ({ name, picture }) => {
+const ProjectItem: React.FC<Props> = ({ name, pictures }) => {
+  const [popupActive, setPopupActive] = useState(false);
+
   return (
-    <article className={styles.project}>
-      <wired-image src={picture} alt={name} />
+    <article onClick={() => setPopupActive(!popupActive)} className={styles.project}>
+      <wired-image src={pictures[0]} alt={name} />
       <h4 className={styles.projectName}>{name}</h4>
+      {popupActive &&
+        <div className={styles.projectPopup}>
+          <div onClick={e => e.stopPropagation()} className={styles.projectPopupContent}>
+            <h5 className={styles.projectPopupContentHeader}>
+              <span>{name}</span>
+              <span onClick={() => setPopupActive(false)} className={styles.projectPopupContentHeaderCross}>X</span>
+            </h5>
+            <div className={styles.projectPopupContentPictures}>
+              {pictures.map((picture: string, i: number) =>
+                <wired-image key={i} src={picture} alt={`foto ${name} ${i}`} class={styles.projectPopupContentPicturesItem} />
+              )}
+            </div>
+          </div>
+        </div>
+      }
     </article>
   )
 }
--- src/sections/Projects/index.tsx
+++ src/sections/Projects/index.tsx
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import Section from 'components/Section';
 import styles from './Projects.module.scss';
 import ProjectItem from './ProjectItem';
 import img from 'img/placeholder.png';
-import Carousel from '@brainhubeu/react-carousel';
-import '@brainhubeu/react-carousel/lib/style.css';
 
 const ProjectsSection: React.FC = () => {
+  const carousel = useRef<HTMLDivElement>(null);
+
+  const scroll = (back: boolean = false) => {
+    if(carousel.current && carousel.current.firstElementChild) {
+      const windowWidth: number = window.innerWidth > 1280 ? 1280 : window.innerWidth;
+      const width: number = carousel.current.firstElementChild.clientWidth + (windowWidth / 160);
+      const distance: number = back ? -width : width;
+
+      carousel.current.scroll({
+        behavior: 'smooth',
+        left: carousel.current.scrollLeft + distance,
+      });
+    }
+  }
+
   return (
     <Section name='projecten' className={styles.projects}>
       <h3>{'Projecten'}</h3>
       <p className={styles.projectsDescription}>{'Het afgelopen jaar hebben wij al (aantal) tuinen ontworpen en hier zijn we trots op. Laat u inspireren en neem eens een kijkje in ons portfolio.'}</p>
-      <Carousel
-        arrowLeft={<FontAwesomeIcon icon={faChevronLeft} className={`${styles.projectsSliderIcon} ${styles.projectsSliderIconBack}`} />}
-        arrowRight={<FontAwesomeIcon icon={faChevronRight} className={`${styles.projectsSliderIcon} ${styles.projectsSliderIconNext}`} />}
-        addArrowClickHandler
-        slidesPerPage={3}
-        offset={+styles.defaultSpacing.slice(0, -2)}
-        infinite
-        autoPlay={3500}
-        stopAutoPlayOnHover
-      >
-        <ProjectItem name={'Tuin in Naaldwijk'} picture={img} />
-        <ProjectItem name={'Tuin in De Lier'} picture={img} />
-        <ProjectItem name={'Tuin in Honselersdijk'} picture={img} />
-        <ProjectItem name={'Tuin in Maasdijk'} picture={img} />
-        <ProjectItem name={'Tuin in Poeldijk'} picture={img} />
-      </Carousel>
+      <div className={styles.projectsCarousel}>
+        <FontAwesomeIcon onClick={() => scroll(true)} icon={faChevronLeft} className={styles.projectsCarouselIcon} />
+        <div ref={carousel} className={styles.projectsCarouselContent}>
+          <ProjectItem name={'Tuin in Naaldwijk'} pictures={[img,img, img]} />
+          <ProjectItem name={'Tuin in De Lier'} pictures={[img,img, img]} />
+          <ProjectItem name={'Tuin in Honselersdijk'} pictures={[img,img, img]} />
+          <ProjectItem name={'Tuin in Maasdijk'} pictures={[img,img, img]} />
+          <ProjectItem name={'Tuin in Poeldijk'} pictures={[img,img, img]} />
+        </div>
+        <FontAwesomeIcon onClick={() => scroll()} icon={faChevronRight} className={styles.projectsCarouselIcon} />
+      </div>
     </Section>
   );
 }
